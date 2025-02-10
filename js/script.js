c1e1535b8c712e1aa48b6f5db9c6871f1787a806
ZELDERooM <denissokolov814@gmail.com>
2025-02-10 16:19:24 +0400

    Start

---
+++ js/script.js
@@ -0,0 +1,33 @@
+document.write("<h2>Первая строка в отдельном файле</h2>")
+document.write("Hello world")
+
+document.write("<h2>First program</h2>");
+
+// document.write("1) " + "2 + 5 = ");
+// sum = 2 + 5;
+// document.write(sum);
+
+
+document.write("<p>HGHGH</p>");
+
+
+// Отправка на консоль
+
+/*
+sum = 5 + 8;
+console.log("Результат операции: ");
+console.log(sum);
+*/
+
+
+// Созание и вывод переменных
+var userName1 = "Tom";
+var userAge1 = 16;
+var isMarried = false;
+document.writeln("<h3>Моё имя " + userName1 + ", мне " + userAge1 + " лет</h3>");
+var text = (`name - ${userName1} 
+           \nage - ${userAge1}
+           \nstatus - ${isMarried}`);
+
+console.log(text)
+
