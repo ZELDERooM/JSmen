c68f4f7941bfd572e4135c13bdc1d3b7c8e285e2
ZELDERooM <denissokolov814@gmail.com>
2025-02-17 16:05:55 +0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -34,3 +34,38 @@ console.log(text)
 document.writeln("<br> тип userName1: " + typeof userName1);
 document.writeln("<br> тип userAge1: " + typeof userAge1);
 
+
+// Операторы ? и ??
+document.write("<h3>Операторы ? и ??</h3>")
+document.write("<p></p>")
+
+var a = 1;
+var b = 2;
+var result = a < b ? a : b;
+document.writeln(`Первое равенство ${result} <br>`);
+
+result = a + 9 < b + 2 ? a : b;
+document.writeln(`Второе равенство ${result} <br>`)
+
+document.write(``)
+
+document.write("<p></p>")
+
+
+
+// parseInt()
+document.write("<h3>parseInt()</h3>")
+document.write("<p></p>")
+
+var num1 = "1234ddd";
+var num2 = parseInt(num1);
+document.write(`parseInt(num1) = ${num2}`);
+
+num1 = "1q2w3e4r";
+num2 = parseInt(num1);
+document.write
+
+
+
+
+document.write("<p></p>")
